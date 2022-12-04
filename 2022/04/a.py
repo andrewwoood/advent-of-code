817a0ff4ff4d86d731e13659701dccc5ec174590
Andrew Wood <23269446+andrewwoood@users.noreply.github.com>
2022-12-04 00:58:18 -0500

    day 4

---
+++ 2022/04/a.py
@@ -0,0 +1,19 @@
+def split_into_range(assignment):
+    res = assignment.split("-")
+    res = set(range(int(res[0]), int(res[1])+1))
+    return res
+
+def is_subset(r1, r2):
+    both = r1.union(r2)
+    return max(len(r1), len(r2)) == len(both)
+
+with open("input.txt") as input_data:
+    count = 0
+
+    pairs = input_data.readlines()
+    for pair in pairs:
+        first, second = pair.split(",")
+        range1, range2 = split_into_range(first), split_into_range(second)
+        if is_subset(range1, range2): count += 1
+
+    print(f"total is {count}")
